d18144648c1ae8f68670886977d6b24712117bf5
Brenda Evilly <165739999+brendaevilly@users.noreply.github.com>
2025-02-20 10:28:39 -0300

    Aprendendo construtores.

--- course/src/entities/Product.java
+++ course/src/entities/Product.java
@@ -4,6 +4,15 @@ public class Product {
 	public String name;
 	public double price;
 	public int quantity;
+	
+	
+	//Usando um construtor.
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
 
 	public double totalValueInStock() {
 		return quantity * price;
