ce48866dfc39fa91a7211a6162e4bd04debeea75
Brenda Evilly <165739999+brendaevilly@users.noreply.github.com>
2025-02-25 00:12:13 -0300

    Estudando matrizes.

---
+++ ex-matriz/src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int lin = sc.nextInt();
+		int col = sc.nextInt();
+		
+		int[][] mat = new int[lin][col];
+		
+		for(int i=0; i<mat.length; i++) {
+			for(int j=0; j<mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		int num = sc.nextInt();
+		
+		for(int i=0; i<mat.length; i++) {
+			for(int j=0; j<mat[i].length; j++) {
+				if(mat[i][j] == num) {
+					System.out.println("Position " + i + ", " + j);
+					if((j - 1 > -1)) {
+						System.out.println("Left: " + mat[i][j-1]);
+					}
+					if((j + 1 < mat[i].length)) {
+						System.out.println("Right: " + mat[i][j+1]);
+					}
+					if((i - 1 > -1)) {
+						System.out.println("Up: " + mat[i-1][j]);
+					}
+					if((i + 1 < mat.length)) {
+						System.out.println("Down: " + mat[i+1][j]);
+					}
+				}
+			}
+			
+		}
+		
+		sc.close();
+
+	}
+
+}
