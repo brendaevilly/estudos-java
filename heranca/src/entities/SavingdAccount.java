5ae56b31c9ce15d214723abc28153fbb665dec7f
Brenda Evilly <165739999+brendaevilly@users.noreply.github.com>
2025-04-03 08:49:06 -0300

    Sobreposição, classe super e @Override.

---
+++ heranca/src/entities/SavingdAccount.java
@@ -0,0 +1,35 @@
+package entities;
+
+public class SavingdAccount extends Account {
+	
+	private Double interestRate;
+	
+	public SavingdAccount() {
+		super();
+	}
+
+	public SavingdAccount(Integer number, String holder, Double balance, Double interestRate) {
+		super(number, holder, balance);
+		this.interestRate = interestRate;
+	}
+
+	public Double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	public void updateBalance() {
+		balance += balance * interestRate;
+	}
+	
+	// Sobreposição.
+	
+	@Override
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	
+}
